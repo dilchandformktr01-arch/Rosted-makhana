72c26722f9f6fae49f184310f98d56d2f48c6ba2
dilchandformktr01-arch <dilchandformktr01@gmail.com>
2026-05-16 13:03:39 +0530

    Add files via upload

---
+++ index.html/script.js
@@ -0,0 +1,15 @@
+console.log("Didi Ka Makhana Website Loaded");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.home(btn => {
+  btn.addEventListener("mouseenter", () => {
+    btn.style.transform = "scale(1.05)";
+    btn.style.transform
+     UP;
+  });
+
+  btn.addEventListener("mouseleave", () => {
+    btn.style.transform = "scale(1)";
+  });
+});
